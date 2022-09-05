5c744d9fdff7cf9fa11adab54a5ad2fc061f7433
Touheda812 <57848483+Touheda812@users.noreply.github.com>
2022-09-05 12:15:23 -0400

    Add files via upload

---
+++ The Python Quiz Game.py
@@ -0,0 +1,37 @@
+# How to make a Quiz Game using python
+
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play!!")
+score = 0
+
+answer = input("What does CPU stands for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stands for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stands for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 3) * 100) + "% ")
+
+
+
