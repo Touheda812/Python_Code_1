76476ad686013d3cf8834198bea172a960c442a0
Touheda812 <57848483+Touheda812@users.noreply.github.com>
2022-09-08 22:19:29 -0400

    Add files via upload

---
+++ A game.py
@@ -0,0 +1,15 @@
+#This is another way of commenting 3 quotations
+'''
+data types in python: String, int, float, bool
+string: "hello", "hi"
+int: 9, -9
+float: 0.9
+bool: True, False
+'''
+
+print("Welcome to my first game!")
+name = input("What is your name? ")
+age = input("What is your age? ")
+print("Hello", name, "you are", age, "years old.")
+
+
